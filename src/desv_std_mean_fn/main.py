47839be07eba4626d8ad45ad937d269e7c6f0d8d
aacevedoaliaga <aacevedoaliagagmail.com>
2024-02-18 23:06:37 -0500

    Se incorporó el manejo de excepciones

--- src/desv_std_mean_fn/main.py
+++ src/desv_std_mean_fn/main.py
@@ -1,6 +1,8 @@
 '''
 Función pra calcular la deviación estandar
 '''
+
+
 def DESV(data):
     mean = sum(data) / len(data)
     squared_diff = [(x - mean) ** 2 for x in data]
@@ -8,9 +10,13 @@ def DESV(data):
     std_dev = variance ** 0.5
     return mean, std_dev
 
-if __name__ == '__main__':
-    input_data = input("Ingresa los datos separados por espacios: ")
+
+#if __name__ == '__main__':
+input_data = input("Ingresa los datos separados por espacios: ")
+try:
     data = [float(x) for x in input_data.split()]
-    media, desv= DESV(data)
+    media, desv = DESV(data)
     print("La Media es:", media)
     print("La Desviación Estándar es:", desv)
+except ValueError:
+    print("Ingrese números por favor")
