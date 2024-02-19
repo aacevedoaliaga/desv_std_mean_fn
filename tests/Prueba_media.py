4a7193c787a238de45ecda7a059f7c98bf9dc6c1
aacevedoaliaga <aacevedoaliagagmail.com>
2024-02-19 02:47:26 -0500

    AZUL: Media y Ds

--- tests/Prueba_media.py
+++ tests/Prueba_media.py
@@ -6,12 +6,24 @@ class Prueba_media(unittest.TestCase):
     def tearDown(self):
         self.ds_media = None
 
-    def test_float_str(self):
+    def test_float(self):
         data = [15.62, 15.90]
         resultadoActual = self.ds_media.DStandar_Mean(data)
         resultadoesperado = (15.760, 0.140)
         self.assertEqual(resultadoesperado, resultadoActual)
 
+    def test_negativo(self):
+        data = [15.00,16.06,15.14,-20.00,17.25,14.37,14.28 ]
+        resultadoActual = self.ds_media.DStandar_Mean(data)
+        resultadoesperado = (10.300,12.407)
+        self.assertEqual(resultadoesperado, resultadoActual)
+
+    def test_integer_str(self):
+        data = [15.00, 16.06, 15.14, -20.00, 17.25, 14.37, 14.28]
+        data_with_str = ["a", 16.06, 15.14, -20.00, 17.25, 14.37, 14.28]
+        self.assertListEqual(data, data_with_str)
+        with self.assertRaises(ValueError):
+            self.ds_media.DStandar_Mean(data_with_str)
 
 
 
